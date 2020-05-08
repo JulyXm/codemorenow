b224aee884838f198b9c7c8a854692af239bb685
vzxm <292648912@qq.com>
2020-05-08 23:37:14 +0800

    init

---
+++ codemore/practice/21.合并两个有序链表.cs
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=csharp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+ public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+ }
+
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
+// @lc code=end
+
